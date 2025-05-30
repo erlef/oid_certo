103b25e2d9b25a8baa6bab55a621e136124f4a9a
Jonatan Männchen <jonatan@maennchen.ch>
2025-05-30 14:01:11 +0200

    Release v0.1.0-beta.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OIDCerto.MixProject do
   def project do
     [
       app: :oid_certo,
-      version: "0.1.0-beta.1",
+      version: "0.1.0-beta.2",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
