cffce4aff7499140b91178055121830f6420f087
Jonatan Männchen <jonatan@maennchen.ch>
2025-06-02 17:07:59 +0200

    Safely Fail if Implementation Log is terminated

--- config/config.exs
+++ config/config.exs
@@ -2,4 +2,4 @@ import Config
 
 config :logger, :default_formatter,
   format: "[$level] $message $metadata\n",
-  metadata: [:plan, :plan_id, :test, :test_id, :internal_id, :os_pid, :device]
+  metadata: [:plan, :plan_id, :test, :test_id, :internal_id, :os_pid, :device, :error]
